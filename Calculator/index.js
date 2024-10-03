5b5d8786e47b46c836705f3d750061ee147e3f8e
poojapdarade <poojap.darade@gmail.com>
2024-10-03 23:57:48 +0100

    Work in progress- Calculator Typescript

--- Calculator/index.js
+++ Calculator/index.js
@@ -4,6 +4,14 @@ exports.main = main;
 var readline_sync_1 = require("readline-sync");
 function main() {
     var firstString = (0, readline_sync_1.question)("Enter first number: \n");
-    console.log(firstString);
+    var operator = (0, readline_sync_1.question)("Enter operator: \n");
+    var secondString = (0, readline_sync_1.question)("Enter second number: \n");
+    var firstNum = isNumber(firstString);
+    console.log(firstNum);
+}
+function isNumber(str) {
+    var mayBeNumber = parseInt(str);
+    var isNum = Boolean(!isNaN(mayBeNumber));
+    return isNum;
 }
 main();
--- Calculator/index.ts
+++ Calculator/index.ts
@@ -2,7 +2,30 @@ import { question } from "readline-sync";
 
 export function main(): void {
   const firstString: string = question("Enter first number: \n");
-  console.log(firstString);
+  const operator: string = question("Enter operator: \n");
+  const secondString: string = question("Enter second number: \n");
+
+  const firstNum = isNumber(firstString);
+  console.log(firstNum);
+}
+
+function isOperator(operator: string): boolean {
+  return operator==="+"
+  switch (operator) {
+  case "+";
+  case "-";
+  case "*";
+  case "/";
+  return true; 
+
+  
+}
+}
+
+function isNumber(str: string): boolean {
+  const mayBeNumber = parseInt(str);
+  const isNum: boolean = !isNaN(mayBeNumber);
+  return isNum;
 }
 
 main();
