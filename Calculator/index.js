f3c843448d7eadf09888b4ad25cf8a9ebd05fdb7
poojapdarade <poojap.darade@gmail.com>
2024-10-05 00:00:31 +0100

    Calculator is working well using typescript

--- Calculator/index.js
+++ Calculator/index.js
@@ -6,8 +6,31 @@ function main() {
     var firstString = (0, readline_sync_1.question)("Enter first number: \n");
     var operator = (0, readline_sync_1.question)("Enter operator: \n");
     var secondString = (0, readline_sync_1.question)("Enter second number: \n");
-    var op = isOperator(operator);
-    console.log(op);
+    var validInput = isNumber(firstString) && isOperator(operator) && isNumber(secondString);
+    console.log(validInput);
+    if (validInput) {
+        console.log("is valid");
+        var firstNum = parseInt(firstString);
+        var secondNum = parseInt(secondString);
+        var result = calculate(firstNum, operator, secondNum);
+        console.log(result);
+    }
+    else {
+        console.log("Invalid Input \n");
+        main();
+    }
+}
+function calculate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case "+":
+            return firstNum + secondNum;
+        case "-":
+            return firstNum - secondNum;
+        case "*":
+            return firstNum * secondNum;
+        case "/":
+            return firstNum / secondNum;
+    }
 }
 function isOperator(operator) {
     switch (operator) {
--- Calculator/index.ts
+++ Calculator/index.ts
@@ -1,12 +1,38 @@
 import { question } from "readline-sync";
+type Operator = "+" | "-" | "*" | "/";
 
 export function main(): void {
   const firstString: string = question("Enter first number: \n");
   const operator: string = question("Enter operator: \n");
   const secondString: string = question("Enter second number: \n");
 
-  const op = isOperator(operator);
-  console.log(op);
+  const validInput: boolean =
+    isNumber(firstString) && isOperator(operator) && isNumber(secondString);
+  console.log(validInput);
+
+  if (validInput) {
+    console.log("is valid");
+    const firstNum: number = parseInt(firstString);
+    const secondNum: number = parseInt(secondString);
+    const result = calculate(firstNum, operator as Operator, secondNum);
+    console.log(result);
+  } else {
+    console.log("Invalid Input \n");
+    main();
+  }
+}
+
+function calculate(firstNum: number, operator: string, secondNum: number) {
+  switch (operator) {
+    case "+":
+      return firstNum + secondNum;
+    case "-":
+      return firstNum - secondNum;
+    case "*":
+      return firstNum * secondNum;
+    case "/":
+      return firstNum / secondNum;
+  }
 }
 
 function isOperator(operator: string): boolean {
