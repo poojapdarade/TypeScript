01088cfd7e7e0d8c9247b0d6edfdedf192556c1d
poojapdarade <poojap.darade@gmail.com>
2024-10-08 12:46:13 +0100

    Initial functions declared, need to use them as I progress with the logic. Progress bar is not working at the moment, need to solve the issue soon

--- ShoppingCart/shopping-cart/src/App.tsx
+++ ShoppingCart/shopping-cart/src/App.tsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
-import{ LinearProgress,Grid,Drawer,Badge}  from "@material-ui/core";
+import{ Grid,Drawer,Badge, LinearProgress}  from "@material-ui/core";
+
 import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
 
+import { Wrapper } from "./App.styles";
 
+export type CartItemType={
+  id:Number;
+  category:String;
+  description:String;
+  image:String;
+  price:Number;
+  title:String;
+  amount:Number;
+}
+
+const getProducts= async():Promise<CartItemType[]>=> await(await fetch("https://fakestoreapi.com/products")).json();
 
 export function App() {
+
+  const {data,isLoading,error}=useQuery<CartItemType[]>("products",getProducts);
+  console.log(data);
+
+const getTotalItems=()=>null;
+
+const handleAddToCart=()=>null;
+
+const handleRemoveFromCart=()=>null;
+
+if(isLoading) return <LinearProgress/>;
+if(error) return <div>Something is wrong...!</div>;
+
   return (
     <div className="App">
       Start
